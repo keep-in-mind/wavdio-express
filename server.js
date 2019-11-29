b1711fe3ea4eb789e249888460201f26481cce72
Tobias Uhmann <tobiasuhmann@gmail.com>
2019-11-29 21:29:43 +0100

    Rename vars

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const server = express()
 
 const port = 3000
 
-server.get('/hello', (req, res) => res.send('Hello world!'))
+server.get('/hello', (request, response) => response.send('Hello world!'))
 
 server.listen(port, () => console.log(`Server listening on port ${port}`))
