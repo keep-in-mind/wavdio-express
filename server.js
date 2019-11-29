697bab7aebb28cef886a8bacb85bdaecf5eb9d14
Tobias Uhmann <tobiasuhmann@gmail.com>
2019-11-29 20:49:54 +0100

    Add server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const server = express()
+
+const port = 3000
+
+server.get('/hello', (req, res) => res.send('Hello world!'))
+
+server.listen(port, () => console.log(`Server listening on port ${port}`))
