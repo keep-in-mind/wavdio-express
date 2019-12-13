35cd941662646ecfc94a116106a213f55e5ab7e9
Tobias Uhmann <tobiasuhmann@gmail.com>
2019-12-13 15:08:12 +0100

    Throw away pipeline and linter

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const server = express()
-
-const port = 3000
-
-server.get('/hello', (request, response) => response.send('Hello world!'))
-
-server.listen(port, () => console.log(`Server listening on port ${port}`))
