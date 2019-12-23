60b520a6499b6b0b88b1e203012ab808fc27705e
Tobias Uhmann <tobiasuhmann@gmail.com>
2019-12-23 03:13:45 +0100

    Run migration before server start

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
 
+const migrate = require('migrate')
+
 const app = require('../app');
 
-app.listen();
+migrate.load({
+  stateStore: '.migrate'
+}, function (err, set) {
+  if (err) {
+    throw err
+  }
+  set.up(function (err) {
+    if (err) {
+      throw err
+    }
+    console.log('migrations successfully ran')
+
+    app.listen();
+  })
+})
 
 module.exports = app.server;
--- migrations/1577065560618-30_welcome_page.js
+++ migrations/1577065560618-30_welcome_page.js
@@ -1,9 +1,13 @@
 'use strict'
 
 module.exports.up = function (next) {
+  console.log('Upgrading to 30_welcome_page')
+
   next()
 }
 
 module.exports.down = function (next) {
+  console.log('Downgrading from 30_welcome_page')
+
   next()
 }
