3eeb44d51d0d5fc1ed648896769b4170e2619e98
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-01-06 18:06:41 +0100

    Replace mongoose with mongodb for migration

--- migrations/1577065560618-30_welcome_page.js
+++ migrations/1577065560618-30_welcome_page.js
@@ -1,12 +1,27 @@
-const mongoose = require('mongoose')
+const mongodb = require('mongodb')
 
-const Museum = require('../models/museum')
+const MongoClient = mongodb.MongoClient
+const url = 'mongodb://localhost:27017/wAVdioDB'
 
 module.exports.up = async function () {
   console.log('Upgrading to 30_welcome_page')
 
-  const museums = await Museum.find()
-  console.log(museums)
+  try {
+    const db = await MongoClient.connect(url)
+    const dbo = db.db('wAVdioDB')
+
+    const museum = await dbo.collection('museums').findOne({})
+    const logo = museum.logo
+
+    await dbo.collection('museums').updateOne({}, {$unset: {'logo': null}})
+    await dbo.collection('museums').updateOne({}, {$set: {'contents.$[].logo': null}})
+    await dbo.collection('museums').updateOne({}, {$set: {'contents.$[].image': logo}})
+
+    const museumCheck = await dbo.collection('museums').findOne({})
+
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 module.exports.down = async function (next) {
