56fb77c1bf0f1ae54ba8d96cae0a773116f75ded
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-03-05 19:42:16 +0100

    Replace old POST log endpoints

--- routes/logging.js
+++ routes/logging.js
@@ -6,28 +6,29 @@ const fs = require('fs');
 
 router.use(fileUpload({ createParentPath: true }));
 
-router.get("/:_type", (request, response) => {
-  
+router.get("/logs/:_type", (request, response) => {
+
   const _type = request.params._type;
   filename = 'logs/'+_type+'.log';
-  
+
   fs.readFile(filename, function(err, content) {
       if (err) {
         logger.error(err);
         return response.status(500).send(err);
-      }      
+      }
       response.status(200).send(JSON.stringify(content.toString('utf-8')));
-    });  
+    });
 });
 
-router.post("/info", (request, response) => {
-    logger.info(request.body.log);    
-    response.status(201).send();
-});
+router.route('/logs').post((request, response) => {
+  const message = request.body;
+  if (message.level < 4) {
+    logger.info(message);
+  } else {
+    logger.error(message);
+  }
 
-router.post("/error", (request, response) => {    
-    logger.error(request.body.log);    
-    response.status(201).send();
+  response.status(201).send();
 });
 
 module.exports = router;
