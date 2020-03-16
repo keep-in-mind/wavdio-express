da50066e84a2fffee158004cad7ba98ce9763c57
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-03-16 01:13:25 +0100

    Add imprint to museum-content model

--- models/schemas/museum-content.js
+++ models/schemas/museum-content.js
@@ -12,6 +12,7 @@ module.exports = new Schema({
   welcomeText: {type: String, required: false},
   termsOfUse: {type: String, required: false},
   privacyTerms: {type: String, required: false},
+  imprint: {type: String, required: false},
 
   logo: {type: Image, required: false},
   image: {type: Image, required: false},
