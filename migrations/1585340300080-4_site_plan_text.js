d267534989ef487d4562c46a37228c47652d0277
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-10-19 15:15:05 +0200

    Refactor migration 4 (site plan text)

---
+++ migrations/1585340300080-4_site_plan_text.js
@@ -0,0 +1,64 @@
+/*
+ * Support adding an imprint.
+ */
+
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+
+const dbUri = process.env.DB_URI
+
+/* Up */
+
+module.exports.up = async function () {
+  console.log('Upgrading to version 4 (site plan text)')
+
+  try {
+    const db = await MongoClient.connect(dbUri)
+    const dbo = db.db('wavdio-express')
+    const meta = await dbo.collection('meta').findOne()
+
+    if (meta.version === 3) {
+      // nothing to do
+
+      await dbo.collection('meta').updateOne({}, {$set: {'version': 4}})
+    } else {
+      console.warn('Skipping')
+    }
+
+    await db.close()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/* Down */
+
+module.exports.down = async function () {
+  console.log('Downgrading from version 4 (site plan text)')
+
+  try {
+    const db = await MongoClient.connect(dbUri)
+    const dbo = db.db('wavdio-express')
+    const meta = await dbo.collection('meta').findOne()
+
+    if (meta.version === 4) {
+      await down(dbo)
+
+      await dbo.collection('meta').updateOne({}, {$set: {'version': 3}})
+    } else {
+      console.warn('Skipping')
+    }
+
+    await db.close()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function down (dbo) {
+
+  /* Delete site plan text from all languages */
+
+  dbo.collection('museums').updateOne({}, {$unset: {'contents.$[].sitePlanText': ''}})
+}
